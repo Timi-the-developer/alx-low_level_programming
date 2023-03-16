b9490181ae70b17dae56129161e0c059fcce52e0
Timi-the-developer <olagbenrotimilehin@gmail.com>
2023-03-16 16:43:28 -0700

    finally!!!!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - the main function
  *
  * Return: always 0
@@ -10,6 +9,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
