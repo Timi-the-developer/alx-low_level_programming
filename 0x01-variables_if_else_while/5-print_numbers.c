18d03cf6c240da80f9eaf13efdecf890c3d33b6a
Timi-the-developer <olagbenrotimilehin@gmail.com>
2023-03-17 21:15:47 -0700

    created file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry function
+ *
+ * Return: 0 (when successful)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0' ; i <= '9' ; i++)
+		putchar(i);
+	return (0);
+}
