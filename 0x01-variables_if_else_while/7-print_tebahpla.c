d3cfedad50dfd387ce876724f6f540d2de13e69d
Timi-the-developer <olagbenrotimilehin@gmail.com>
2023-03-17 21:39:49 -0700

    created file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; 1 >= 'a' ; --i)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
