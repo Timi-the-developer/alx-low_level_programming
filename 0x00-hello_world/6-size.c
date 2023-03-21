ce6428465c4147053c97ccd6e64c574bf805e7a9
Timi-the-developer <olagbenrotimilehin@gmail.com>
2023-03-20 18:04:09 -0700

    mod file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,9 +16,15 @@ int main(void)
 
 	double w;
 
+	long int x;
+
+	long long int;
+
 	printf("size of int is: %zu\n", sizeof(int));
 	printf("size of char is: %zu\n", sizeof(char));
 	printf("size of float is: %zu\n", sizeof(float));
 	printf("size of double is: %zu\n", sizeof(double));
+	printf("size of long int is: %zu\n", sizeof(long int));
+	printf("size of long long int is: %zu\n", sizeof(long long int));
 	return (0);
 }
