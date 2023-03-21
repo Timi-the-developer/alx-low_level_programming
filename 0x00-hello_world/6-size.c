0170786a4daada175f962d3e34352d4c84e3b54c
Timi-the-developer <olagbenrotimilehin@gmail.com>
2023-03-20 17:58:31 -0700

    completed file 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 (when successul)
+ */
+
+int main(void)
+{
+	int i;
+
+	char c;
+
+	float f;
+
+	double w;
+
+	printf("size of int is: %zu\n", sizeof(int));
+	printf("size of char is: %zu\n", sizeof(char));
+	printf("size of float is: %zu\n", sizeof(float));
+	printf("size of double is: %zu\n", sizeof(double));
+	return (0);
+}
