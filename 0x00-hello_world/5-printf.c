c9107946fae98e5cac91a598adf71b07c44d48a1
Timi-the-developer <olagbenrotimilehin@gmail.com>
2023-03-20 17:09:25 -0700

    completed file 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	printf("\n");
+	return (0);
+}
