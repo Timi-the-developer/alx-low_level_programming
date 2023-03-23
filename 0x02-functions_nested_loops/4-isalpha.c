328830bf81d4b8354fe97c52ff5ea1345c6e6476
Timi-the-developer <olagbenrotimilehin@gmail.com>
2023-03-22 17:35:08 -0700

    created file 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - function to check for alphabet
+ *
+ * @c : perimeter to be checked
+ *
+ * Return: 1 if it is an alphabet
+ * and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if
+		((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
