cb340a5b33642e68eb2db84505be01bddec55deb
Timi-the-developer <olagbenrotimilehin@gmail.com>
2023-03-27 16:15:55 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ *@s: variable of string length
+ *Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
