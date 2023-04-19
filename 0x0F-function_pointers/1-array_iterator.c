5b1120f41523a4a7061c282b6deb1c93e0b22d3b
Timi-the-developer <olagbenrotimilehin@gmail.com>
2023-04-19 14:10:46 -0700

    array-iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (!array || !action)
+	{
+		return;
+	}
+
+	for (size_t i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
